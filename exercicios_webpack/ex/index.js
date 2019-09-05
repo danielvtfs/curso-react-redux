663492397410c9ddc270ae9d327a03eb668eee91
Daniel Vinicius Teixeira Fernandez <daniel.fernandez@unigranrio.br>
2019-09-05 14:27:45 -0300

    Aula 13 - Interpretando o Operador Spread Plugin Babel

--- exercicios_webpack/ex/index.js
+++ exercicios_webpack/ex/index.js
@@ -1,4 +1,11 @@
-import Pessoa from './pessoa'
-
-const pessoa = new Pessoa('Guilherme')
-console.log(pessoa.toString())
+const produto = {
+    nome: 'Caneta Bic Preta',
+    preco: 1.90,
+    desconto: 0.05
+  }
+  function clone(objeto) {
+    return { ...objeto }
+  }
+  const novoProduto = clone(produto)
+  novoProduto.nome = 'Caneta Bic Azul'
+  console.log(produto, novoProduto)
