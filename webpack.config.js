712019156214c6bb597d4034028ff8e2235e977f
Daniel Vinicius Teixeira Fernandez <daniel.fernandez@unigranrio.br>
2019-09-06 08:46:14 -0300

    Aula 17 - Configurando o Build com Webpack

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const webpack = require('webpack')
+
+module.exports = {
+    entry: __dirname + './ex/index.js',
+    output:{
+        path: __dirname + '/public',    
+        filename: './bundle.js'
+    },
+    devServer: {
+        port: 8080,
+        contentBase: './public'
+    },
+    module: {
+        loaders: [{
+            test: /.jsx?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['es2015', 'react']
+            }
+        }]
+    }
+      
+}
