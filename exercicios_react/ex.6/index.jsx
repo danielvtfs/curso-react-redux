61b15ba34a29879701efeca37bc15418147b2d55
Daniel Vinicius Teixeira Fernandez <daniel.fernandez@unigranrio.br>
2019-09-13 16:41:14 -0300

    Aula 25 - Componentes Filhos

---
+++ exercicios_react/ex.6/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import SIlvaFamily from './silvaFamily'
+
+ReactDOM.render(
+    <SIlvaFamily />
+    , document.getElementById('app'))
+
+
--- exercicios_react/ex/index.jsx
+++ exercicios_react/ex/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import SIlvaFamily from './silvaFamily'
+import Family from './family'
+import Member from './member'
+
 
 ReactDOM.render(
-    <SIlvaFamily />
+    <Family lastName='Silva'>
+        <Member name='Guilherme'/>
+    </Family>
     , document.getElementById('app'))
 
 
